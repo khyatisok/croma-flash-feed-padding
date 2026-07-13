6b2956f90edede090b061c83dc17d659bfc1990a
khyatisok <khyati.srivastava@dentsu.com>
2026-07-13 17:07:44 +0530

    Update filter_feed.py

--- filter_feed.py
+++ filter_feed.py
@@ -210,7 +210,7 @@ with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
 tree = etree.ElementTree(root)
 
 tree.write(
-    "flash_sale_feed.xml",
+    "flash_sale_feed_padding.xml",
     encoding="utf-8",
     xml_declaration=True,
     pretty_print=True
